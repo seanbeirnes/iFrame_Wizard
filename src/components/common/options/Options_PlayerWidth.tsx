ed1af819c2cb70eb6e107d633f7e7ec16c0d8216
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-04 02:24:54 -0400

    Disabled height option in player maximum size.

--- src/components/common/options/Options_PlayerWidth.tsx
+++ src/components/common/options/Options_PlayerWidth.tsx
@@ -27,7 +27,7 @@ export default function Options_PlayerWidth({ label, id, value, width, height, c
                         </div>
                         <div>
                             <label className="text-xs text-gray-700 pr-2">Height</label>
-                            <input id={id+"_height"} className="px-1 w-16 mr-2 border-2 border-gray-200 rounded-md shadow-inner focus:border-sky-400 outline-2 outline-sky-400 text-xs text-gray-500" type="number" placeholder="315" min="0" max="1080" step="10" value={height} onChange={changeHandler} ></input>
+                            <input id={id+"_height"} className="px-1 w-16 mr-2 border-2 border-gray-200 rounded-md shadow-inner focus:border-sky-400 outline-2 outline-sky-400 text-xs text-gray-500" type="number" placeholder="315" min="0" max="1080" step="10" value={height} onChange={changeHandler} disabled></input>
                         </div>
                         
                     </div>
