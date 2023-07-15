2cf9bd9d14d317a0b29a33fb115d00a2263ac751
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-14 20:27:48 -0400

    Addeed opacity on hover to square button.

--- src/components/common/buttons/ButtonSquare/ButtonSquare.tsx
+++ src/components/common/buttons/ButtonSquare/ButtonSquare.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function ButtonSquare({ clickHandler, src }: Props){
 
     return (
-        <button type="button" className="w-12 h-12 p-1 rounded-md bg-white border-gray-50 border-2 shadow hover:shadow-md hover:bg-gray-50 active:shadow-inner transition outline-2 outline-sky-400" onClick={ clickHandler }>
+        <button type="button" className="w-12 h-12 p-1 rounded-md bg-white border-gray-50 border-2 shadow hover:shadow-md hover:bg-gray-5 hover:opacity-70 active:shadow-inner transition outline-2 outline-sky-400" onClick={ clickHandler }>
             <img src={src} />
         </button>
     )
