2177daa5ce7972c0a308a152cc87f116c8dbd2b9
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-23 18:29:18 -0400

    Added mic and camera to util format func.

--- src/models/common/utils.ts
+++ src/models/common/utils.ts
@@ -36,9 +36,19 @@ export function formatWidth(settingsObject: FormPropsData){
 export function formatEmbedCode(settingsObject: FormPropsData){
     const accelerometer = settingsObject.allow_accelerometer ? "accelerometer;" : "";
     const autoplay = settingsObject.allow_autoplay ? "autoplay;" : "";
+
+    const camera_allowed = settingsObject.allow_camera ? "camera" : "";
+    const camera_allow_list = settingsObject.allow_camera_allow_list
+    const camera = camera_allow_list ? `camera ${camera_allow_list}` : camera_allowed;
+
     const clipboard_write = settingsObject.allow_clipboard_write ? "clipboard-write;" : "";
     const encrypted_media = settingsObject.allow_encrypted_media ? "encrypted-media;" : "";
     const gyroscope = settingsObject.allow_gyroscope ? "gyroscope;" : "";
+
+    const microphone_allowed = settingsObject.allow_microphone ? "microphone" : "";
+    const microphone_allow_list = settingsObject.allow_microphone_allow_list
+    const microphone = camera_allow_list ? `microphone ${microphone_allow_list}` : microphone_allowed;
+
     const picture_in_picture = settingsObject.allow_picture_in_picture ? "picture-in-picture;" : "";
     const web_share = settingsObject.allow_web_share ? "web-share;" : "";
     const fullscreen = settingsObject.allow_fullscreen ? "fullscreen;" : "";
@@ -48,7 +58,7 @@ export function formatEmbedCode(settingsObject: FormPropsData){
     return (
     `<div style="max-width: ${player_max_size_width};">
         <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
-            <iframe style="border:none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" title="${settingsObject.name}" src="${settingsObject.url}" width="560" height="315" allow="${accelerometer} ${autoplay} ${clipboard_write} ${encrypted_media} ${fullscreen} ${gyroscope} ${picture_in_picture} ${web_share} ${custom_props}" ${fullscreen && "allowfullscreen"}></iframe>
+            <iframe style="border:none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" title="${settingsObject.name}" src="${settingsObject.url}" width="560" height="315" allow="${accelerometer} ${autoplay} ${camera} ${clipboard_write} ${encrypted_media} ${fullscreen} ${gyroscope} ${microphone} ${picture_in_picture} ${web_share} ${custom_props}" ${fullscreen && "allowfullscreen"} ${(camera || microphone) && "allowusermedia"}></iframe>
         </div>
     </div>`
     )
