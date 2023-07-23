47f00d7e8a49c71624110f682966e62d454e38d6
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-23 17:57:34 -0400

    Added slide in animation to options.

--- src/components/common/options/OptionsWrapper/OptionsWrapper.tsx
+++ src/components/common/options/OptionsWrapper/OptionsWrapper.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function OptionsWrapper({ children }: Props){
     return (
-        <div className="p-2 flex flex-wrap justify-between items-center rounded bg-white shadow hover:shadow-md hover:bg-gray-50 transition">
+        <div className="p-2 flex flex-wrap justify-between items-center rounded bg-white shadow hover:shadow-md hover:bg-gray-50 transition animate-slide-in">
             {children}
         </div>
     )
