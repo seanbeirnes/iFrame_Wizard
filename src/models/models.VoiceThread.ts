959187b3535e031976d412da41e329e84191463a
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-23 19:31:47 -0400

    Added custom model.

--- src/models/models.VoiceThread.ts
+++ src/models/models.VoiceThread.ts
@@ -2,7 +2,7 @@ import { FormPropsData } from "../types/types";
 import { formatResponse, formatEmbedCode } from "./common/utils";
 
 function validateURL(url: string){
-    const reURL = new RegExp(/(https?:\/\/[a-z0-9-]+\.voicethread\.com)\/share\/([0-9]{7,11})/)
+    const reURL = new RegExp(/(https?:\/\/[a-z0-9\-]+\.voicethread\.com)\/share\/([0-9]{7,11})/)
     const matchURL = url.match(reURL)
     if(matchURL){
         return {urlStem: matchURL[1], id: matchURL[2]}
@@ -18,7 +18,7 @@ export default function modelsVoicethread(settingsObject: FormPropsData){
     if(!matchResult){
         return formatResponse(false, "URL or ID invalid", "")
     }
-    // Need to add camera and microphone to "allow" options with optional url or none as additional values. Do not need to add "allowusermedia" it is a deprecated attribute.
+    
     settingsObject.name = "VoiceThread player";
     settingsObject.url = `${matchResult.urlStem}/app/player/?threadId=${matchResult.id}`;
 
--- src/models/models.custom.ts
+++ src/models/models.custom.ts
@@ -1,7 +1,21 @@
 import { FormPropsData } from "../types/types";
-import { formatResponse } from "./common/utils";
+import { formatResponse, formatEmbedCode } from "./common/utils";
+
+function validateURL(url: string){
+    const reURL = new RegExp(/^(?:https?:\/\/)(?:[a-z0-9\-]+\.){1,}[a-z]{2,}(?:\/.*)?$/)
+    const matchURL = url.match(reURL)
+    return matchURL ? true : false;
+}
 
 export default function modelsCustom(settingsObject: FormPropsData){
-    const url = settingsObject.url
-    return formatResponse(true, "operation succeded", url + "Custom")
+
+    const matchResult = validateURL(settingsObject.url)
+
+    if(!matchResult){
+        return formatResponse(false, "URL is invalid.", "")
+    }
+
+    const embedCode = formatEmbedCode(settingsObject)
+
+    return formatResponse(true, "operation succeeded", embedCode)
 }
