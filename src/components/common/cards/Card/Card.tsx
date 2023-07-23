b8f0b82bed8bba5a355e45abe232289c88c161ad
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-23 18:06:06 -0400

    Add slide in animation to cards.

--- src/components/common/cards/Card/Card.tsx
+++ src/components/common/cards/Card/Card.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function Card({ title, children }: Props){
     return (
-        <section className="shadow-md rounded-md m-2 mb-4">
+        <section className="shadow-md rounded-md m-2 mb-4 animate-slide-in">
             <div className="bg-sky-800 rounded-t-md p-2">
                 <h2 className="text-base font-medium text-gray-50 text-center">{title}</h2>
             </div>
